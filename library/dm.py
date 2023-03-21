ec172b437ef1911e89fe800ae5260ec4b552a8b8
danorel <mail.ordan@gmail.com>
2023-03-21 20:30:28 +0200

    Add auto-ml PyCaret classifier to make difficulty predictions

--- library/dm.py
+++ library/dm.py
@@ -9,7 +9,6 @@ DIFFICULTY_COLUMN = 'difficulty'
 
 class Difficulty(Enum):
     HARD = 'hard'
-    MEDIUM = 'medium'
     EASY = 'easy'
     UNKNOWN = 'unknown'
     CONFLICT = 'conflict'
@@ -58,7 +57,6 @@ def split_by_binary(df: pd.DataFrame,
     b_series = pd.Series(data=pd.Categorical(values=[Difficulty.UNKNOWN.value] * len(df),
                                              categories=[
                                                  Difficulty.EASY.value,
-                                                 Difficulty.MEDIUM.value,
                                                  Difficulty.HARD.value,
                                                  Difficulty.UNKNOWN.value,
                                                  Difficulty.CONFLICT.value
@@ -118,7 +116,7 @@ def normalize_numerical(df: pd.DataFrame):
 
 
 def meassure_difficulty_numerical(df: pd.DataFrame, column: str):
-    q_percentage = [0, .16, .33, .66, .84, 1]
+    q_percentage = [0, .2, .8, 1]
     q_values = [df[column].quantile(percentage) for percentage in q_percentage]
 
     q_pairs = []
@@ -133,7 +131,6 @@ def split_by_quantiles(df: pd.DataFrame,
     q_series = pd.Series(data=pd.Categorical(values=[Difficulty.UNKNOWN.value] * len(df),
                                              categories=[
                                                  Difficulty.EASY.value,
-                                                 Difficulty.MEDIUM.value,
                                                  Difficulty.HARD.value,
                                                  Difficulty.UNKNOWN.value,
                                                  Difficulty.CONFLICT.value
@@ -142,9 +139,7 @@ def split_by_quantiles(df: pd.DataFrame,
 
     q_difficulties = [
         Difficulty.HARD.value,
-        Difficulty.MEDIUM.value,
         Difficulty.EASY.value,
-        Difficulty.MEDIUM.value,
         Difficulty.HARD.value,
     ]
 
@@ -177,21 +172,17 @@ def split_by_difficulty_numerical(df: pd.DataFrame):
         cp_df.iloc[cp_nan_index, cp_df.columns.get_loc(
             DIFFICULTY_COLUMN)] = Difficulty.UNKNOWN.value
 
-        def merge_conflicts(a: Difficulty, b: Difficulty, c: Difficulty):
-            ab_index = cp_df.loc[
-                (cp_df[DIFFICULTY_COLUMN] == a.value) |
-                (cp_df[DIFFICULTY_COLUMN] == b.value)
-            ].index
+        def merge_conflicts(a: Difficulty, b: Difficulty):
+            a_index = cp_df.loc[cp_df[DIFFICULTY_COLUMN] == a.value].index
 
-            q_ab_series = q_series.iloc[ab_index]
-            q_abc_series = q_ab_series.loc[q_ab_series == c.value]
+            b_a_series = q_series.iloc[a_index]
+            b_ab_series = b_a_series.loc[b_a_series == b.value]
 
-            cp_df.iloc[q_abc_series.index, cp_df.columns.get_loc(
+            cp_df.iloc[b_ab_series.index, cp_df.columns.get_loc(
                 DIFFICULTY_COLUMN)] = Difficulty.CONFLICT.value
 
-        merge_conflicts(Difficulty.EASY, Difficulty.MEDIUM, Difficulty.HARD)
-        merge_conflicts(Difficulty.HARD, Difficulty.MEDIUM, Difficulty.EASY)
-        merge_conflicts(Difficulty.HARD, Difficulty.EASY, Difficulty.MEDIUM)
+        merge_conflicts(Difficulty.HARD, Difficulty.EASY)
+        merge_conflicts(Difficulty.EASY, Difficulty.HARD)
 
     return cp_df
 
@@ -204,10 +195,8 @@ def meassure_difficulty_categorical(df: pd.DataFrame, column: str):
     c_pairs = []
     for c_value in c_values:
         c_rate = len(df.loc[df[column] == c_value]) / len(df)
-        if (c_rate < .1):
+        if (c_rate < .2):
             c_pairs.append((Difficulty.HARD.value, c_value))
-        elif (c_rate < .25):
-            c_pairs.append((Difficulty.MEDIUM.value, c_value))
         else:
             c_pairs.append((Difficulty.EASY.value, c_value))
 
@@ -220,7 +209,6 @@ def split_by_categoricals(df: pd.DataFrame,
     c_series = pd.Series(data=pd.Categorical(values=[Difficulty.UNKNOWN.value] * len(df),
                                              categories=[
                                                  Difficulty.EASY.value,
-                                                 Difficulty.MEDIUM.value,
                                                  Difficulty.HARD.value,
                                                  Difficulty.UNKNOWN.value,
                                                  Difficulty.CONFLICT.value
@@ -253,20 +241,16 @@ def split_by_difficulty_categorical(df: pd.DataFrame):
         cp_df.iloc[cp_nan_index, cp_df.columns.get_loc(
             DIFFICULTY_COLUMN)] = Difficulty.UNKNOWN.value
 
-        def merge_conflicts(a: Difficulty, b: Difficulty, c: Difficulty):
-            ab_index = cp_df.loc[
-                (cp_df[DIFFICULTY_COLUMN] == a.value) |
-                (cp_df[DIFFICULTY_COLUMN] == b.value)
-            ].index
+        def merge_conflicts(a: Difficulty, b: Difficulty):
+            a_index = cp_df.loc[cp_df[DIFFICULTY_COLUMN] == a.value].index
 
-            q_ab_series = c_series.iloc[ab_index]
-            q_abc_series = q_ab_series.loc[q_ab_series == c.value]
+            b_a_series = c_series.iloc[a_index]
+            b_ab_series = b_a_series.loc[b_a_series == b.value]
 
-            cp_df.iloc[q_abc_series.index, cp_df.columns.get_loc(
+            cp_df.iloc[b_ab_series.index, cp_df.columns.get_loc(
                 DIFFICULTY_COLUMN)] = Difficulty.CONFLICT.value
 
-        merge_conflicts(Difficulty.EASY, Difficulty.MEDIUM, Difficulty.HARD)
-        merge_conflicts(Difficulty.HARD, Difficulty.MEDIUM, Difficulty.EASY)
-        merge_conflicts(Difficulty.HARD, Difficulty.EASY, Difficulty.MEDIUM)
+        merge_conflicts(Difficulty.HARD, Difficulty.EASY)
+        merge_conflicts(Difficulty.EASY, Difficulty.HARD)
 
     return cp_df
